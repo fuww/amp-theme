81514cf268859158272d79e3fcfb0027c6ebfe18
Joost van der Laan <joost@fashionunited.com>
2020-08-12 16:52:00 +0200

    cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -12,31 +12,3 @@ module.exports = {
     'clean-css': {},
   },
 };
-
-// var postcss = require('postcss')
-
-// module.exports = {
-//     use: [
-//         'postcss-import',
-//         'postcss-custom-media',
-//         'postcss-custom-properties',
-//         'postcss-calc',
-//         'postcss-color-function',
-//         'postcss-discard-comments',
-//         'autoprefixer',
-//         'postcss-clean',
-//     ],
-//     // input: 'src/base.css',
-//     // dir: 'css'
-// };
-
-// module.exports = {
-//     plugins: {
-//         autoprefixer: {
-//             browsers: [
-//                 "last 2 versions",
-//                 "Explorer >= 8",
-//             ]
-//         }
-//     },
-// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
 const path = require('path');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const WebpackMd5Hash = require('webpack-md5-hash');
-
-// const devMode = process.env.NODE_ENV !== 'production';
-// const CleanCss = require('clean-css');
 const CssNano = require('cssnano');
 const plugins = require('./webpack/plugins');
 
@@ -17,7 +11,6 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, './'),
-    // publicPath: '/',
     filename: './assets/compiled/[name].js',
   },
   performance: {
@@ -38,47 +31,23 @@ module.exports = {
     },
     minimizer: [
       new UglifyJsPlugin({
-        // cache: true,
         parallel: true,
         sourceMap: true, // set to true if you want JS source maps
       }),
       new OptimizeCSSAssetsPlugin({
-        // assetNameRegExp: /\.css$/g,
-        // assetNameRegExp: /\.html$/g,
         assetNameRegExp: /stylesheet\.html$/g,
-        // cssProcessor: CleanCss,
         cssProcessor: CssNano,
         cssProcessorPluginOptions: {
           preset: ['default', { discardComments: { removeAll: true } }], // for cssnano
-          // preset: ['default', { level: 2 }], // for CleanCSS
-          // options: {
-          //   compatibility: "ie9",
-          //   level: 2,
-          //   inline: ["remote"]
-          // }
         },
       }),
     ],
   },
   plugins: [
     plugins.StyleLintPlugin,
-    // new CleanWebpackPlugin(['static/fonts', 'assets/compiled']),  // Use ONLY for production builds!
     new MiniCssExtractPlugin({
       filename: './layouts/partials/stylesheet.html',
-      // filename: "./assets/compiled/[name].css"
-      // Must use manifest for this:
-      // filename: devMode ? '[name].css' : '[name].[contenthash].css',
     }),
-    // fonts are now duplicate in assets & static folder,
-    // Webpack checks them from the CSS, so not in assets = error
-    // Hugo doesn't copy the files, so not in static = no fonts in frontend
-    // Could not yet get this solution to work: https://discourse.gohugo.io/t/solved-assets-and-fonts/13797/7
-    // new CopyWebpackPlugin([
-    //   {
-    //     from: 'assets/fonts/',
-    //     to: 'static/fonts',
-    //   },
-    // ]),
   ],
   module: {
     rules: [
@@ -90,17 +59,11 @@ module.exports = {
           outputPath: './assets/compiled/fonts/',
         },
       },
-      // {
-      //   test: /^\.\/js\/.*\.js?$/,
-      //   use: ["babel-loader"]
-      // },
       {
         test: /\.(s(a|c)ss)$/,
         use: [
-          // devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
           { loader: MiniCssExtractPlugin.loader },
           { loader: 'css-loader', options: { importLoaders: 1 } },
-          // { loader: 'postcss-loader?sourceMap', options: { config: { path: '../' } } },
           {
             loader: 'postcss-loader?sourceMap',
             options: { config: { path: './' } },
@@ -112,30 +75,6 @@ module.exports = {
             includePaths: ['node_modules'] },
           },
         ],
-
-        // use: ['style-loader',
-        //   { loader: MiniCssExtractPlugin.loader, },
-        //   {
-        //     loader: 'css-loader',
-        //     options: {
-        //       importLoaders: 1
-        //     }
-        //   },
-        //   {
-        //     loader: 'postcss-loader',
-        //     options: {
-        //       config: {
-        //         path: '../'
-        //       }
-        //     }
-        //   //   options: {
-        //   //     plugins: () => [autoprefixer()]
-        //   //  }
-        //   },
-        //   {
-        //     loader: 'sass-loader',
-        //     options: { includePaths: ['node_modules'] }
-        //   }]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
